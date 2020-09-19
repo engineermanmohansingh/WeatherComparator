986b537b42c3bcbdd6ead347a77dcfef1f9739de
Manmohan Singh Virdi <engineermanmohansingh@gmail.com>
2020-09-19 22:36:47 +0530

    Completed phase 3 > Added comparator logic

--- src/test/java/comp/ComparatorTest.java
+++ src/test/java/comp/ComparatorTest.java
@@ -4,14 +4,17 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.TestNGException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -20,15 +23,16 @@ import io.restassured.specification.RequestSpecification;
 
 public class ComparatorTest {
 
-	String chromeDriverPath ;
-	
-	@Test
-	public void TC01_getWeatherDataFromWeb() {
-		
+	String chromeDriverPath, tempFromApi, tempFromWeb;
+	WebDriver driver;
+	WebDriverWait wait;
+	int compTolerance=3;
+	@BeforeClass
+	public void setup() {
 		chromeDriverPath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
-				+ File.separator + "resources" + File.separator + "Drivers" + File.separator + "Mac"
-				+ File.separator + "chromedriver";
-		
+				+ File.separator + "resources" + File.separator + "Drivers" + File.separator + "Mac" + File.separator
+				+ "chromedriver";
+
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 		ChromeOptions options = new ChromeOptions();
 
@@ -36,39 +40,82 @@ public class ComparatorTest {
 		options.addArguments("test-type");
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
-		WebDriver driver = new ChromeDriver(options);
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		
-		
+
+		wait = new WebDriverWait(driver, 30);
+	}
+
+	@Test
+	public void TC01_getWeatherDataFromWeb() {
+
 		driver.get("https://weather.com");
-		
-		WebDriverWait wait = new WebDriverWait(driver, 30);
+		Reporter.log("Navigated to weather.com website", true);
+
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("LocationSearch_input")));
 		WebElement searchField = driver.findElement(By.id("LocationSearch_input"));
 		searchField.sendKeys("Delhi");
+		Reporter.log("Entered city Delhi in Location Search Field", true);
 
-	     wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Delhi']")));
-	     driver.findElement(By.xpath("//button[text()='Delhi']")).click();
-		String temp = driver.findElement(By.xpath("(//h1[contains(.,'Delhi')]/parent::*/following-sibling::*//span[@data-testid='TemperatureValue'])[1]")).getText();
-		System.out.println(temp);
-//		driver.quit();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Delhi']")));
+		driver.findElement(By.xpath("//button[text()='Delhi']")).click();
+
+		tempFromWeb = driver.findElement(By.xpath(
+				"(//h1[contains(.,'Delhi')]/parent::*/following-sibling::*//span[@data-testid='TemperatureValue'])[1]"))
+				.getText().trim();
+		Assert.assertTrue(driver.findElement(By.tagName("h1")).getText().contains("Delhi"),
+				"The result page is not showing expected City name");
+
+		System.out.println(tempFromWeb);
+
 	}
-	
+
 	//@Test
 	public void TC02_getWeatherDataFromApi() {
-		
+
 		RequestSpecification requestObj = RestAssured.given();
 		requestObj.baseUri("https://api.openweathermap.org/");
 		requestObj.basePath("data/2.5/weather");
 		requestObj.queryParam("q", "Delhi,in");
-		requestObj.queryParam("APPID","7fe67bf08c80ded756e598d6f8fedaea");
-		requestObj.queryParam("units","metric");
+		requestObj.queryParam("APPID", "7fe67bf08c80ded756e598d6f8fedaea");
+		requestObj.queryParam("units", "metric");
+		Reporter.log("Request details :- ",true);
+		Reporter.log(requestObj.log().all().toString(),true);
 		requestObj.log().all();
 		Response response = requestObj.get();
 		System.out.println(response.body().asString());
 		System.out.println(response.jsonPath().get("main.temp").toString());
+		Reporter.log("Response retreived :-");
+		Reporter.log(response.body().prettyPrint(),true);
+		tempFromApi = response.jsonPath().get("main.temp").toString();
+
+	}
+
+	@Test
+	public void TC03_compareDataFromTwoSources() {
+
+		Reporter.log("Comparing two temperature values");
+		System.out.println("Temperature extracted from API ="+tempFromApi +" ~ ? ~ Temperature extracted from Web ="+tempFromWeb);
+		int tempA = Integer.valueOf(tempFromApi);
+		int tempW = Integer.valueOf(tempFromWeb.substring(0,2));
+		if((tempA-tempW)==0) {
+			Reporter.log("Two temperature values match exactly");
+		}else {
+			int res = Math.abs(tempA-tempW) ;
+			if(res<compTolerance) {
+				Reporter.log("Two values match within the tolerance range, unit difference observed -"+res+" and tolerance specified -"+compTolerance,true);
+			}else {
+				throw new TestNGException("Two values do not match within the tolerance range, unit difference observed -"+res+" and tolerance specified -"+compTolerance);
+			}
+		}
 		
+		
+		
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
 	}
 }
